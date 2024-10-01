9a069987a1389aea69ef0b7b0b44831d57dab843
Danish <sheikhd@rknec.edu>
2024-10-01 13:41:23 +0530

    Added alternative code

--- src/sorting/QuickSort.java
+++ src/sorting/QuickSort.java
@@ -42,7 +42,32 @@ public class QuickSort {
      return i + 1;  // Return the partition index
  }
 
-
+//Alternative
+ /* static int partition(int arr[], int low, int high)
+    {
+        int pivot = arr[low];
+        int i = low ;
+        int j = high;
+       while(i<j){
+           while(arr[i] <= pivot && i <= high - 1){
+               i++;
+           }
+           while(arr[j]>pivot && j>= low  + 1 ){
+               j--;
+           }
+        if(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            }
+        }
+        
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+        
+        return j;
+    }*/
 
  // Main method to test QuickSort
  public static void main(String[] args) {
